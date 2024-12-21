d13f6cca653ad8b5c5908dd810207ce0d8582d38
Almaz <almaz.bukayev@narxoz.kz>
2024-12-21 16:31:56 +0500

    Удалил ненужные файлы x3

--- cloud_env.bat
+++
@@ -1,7 +0,0 @@
-ECHO OFF
-CLS
-SET PATH=C:\Users\Asus\Desktop\tic\google-cloud-sdk\bin;%PATH%;
-cd C:\Users\Asus\Desktop\tic
-ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
-ECHO ---
-ECHO ON
